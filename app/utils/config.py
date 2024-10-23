eea7fcaa8a511b525fb938d9553c0999d3458b22
PioRow <piotr.rowicki02@gmail.com>
2024-10-23 20:55:09 +0200

    parsing database config

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,5 +1,16 @@
+from dataclasses import dataclass
 from typing import Any
+import os
+import json
 
 
-class Config:
-    _config:dict[str, str]
+class DB_Config:
+    with open("app/resources/db_config.json",'r') as f:
+        __DB_PARAMS=json.load(f)
+    DB_CONFIG ="postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}".format(
+            DB_USER=__DB_PARAMS["DB_USER"],
+            DB_PASSWORD=__DB_PARAMS["DB_PASSWORD"],
+            DB_HOST=__DB_PARAMS["DB_HOST"],
+            DB_NAME=__DB_PARAMS["DB_NAME"],
+        )
+db_config = DB_Config()
